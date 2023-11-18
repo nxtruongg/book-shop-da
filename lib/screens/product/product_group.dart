76c771ef547c285e7366ab2939dd8ce4993eb015
XTruong <nxtruong0811@gmail.com>
2023-11-18 11:56:07 +0700

    add product_screen, change bottom navi

--- lib/screens/product/product_group.dart
+++ lib/screens/product/product_group.dart
@@ -1,67 +1,60 @@
- import 'package:flutter/material.dart';
 import 'package:bookshop/screens/product/product_detail.dart';
-import 'package:bookshop/models/product_object.dart';
-import 'package:bookshop/provider/product_provider.dart';
+import 'package:flutter/material.dart';
 
+import '../../models/product_object.dart';
+import '../../provider/product_provider.dart';
 import '../auth/login_screen.dart';
-
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class ProductGroup extends StatefulWidget {
+  const ProductGroup({super.key});
 
   @override
-  State<StatefulWidget> createState() {
-    return HomeScreenState();
-  }
+  State<ProductGroup> createState() => _ProductGroupState();
 }
 
-class HomeScreenState extends State<HomeScreen> {
+class _ProductGroupState extends State<ProductGroup> {
   List<ProductObject> lsProducts = [];
   void _LoadDanhSach() async {
     final data = await ProductProvider.getAllContacts();
     setState(() {});
     lsProducts = data;
   }
-
   @override
   void initState() {
     // TODO: implemet initState
     super.initState();
     _LoadDanhSach();
   }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xFFBA1541),
-        title: Text('Trang chủ'),
+        title: Text('Danh mục sản phẩm'),
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.notifications),
+            icon: Icon(Icons.search),
             onPressed: () {},
           ),
-          // PopupMenuButton<String>(
-          //   itemBuilder: (BuildContext context) {
-          //     return <PopupMenuEntry<String>>[
-          //       PopupMenuItem(
-          //         value: 'theloai1',
-          //         child: Text('Thể loại 1'),
-          //       ),
-          //       PopupMenuItem(
-          //         value: 'theloai2',
-          //         child: Text('Thể loại 2'),
-          //       ),
-          //       PopupMenuItem(
-          //         value: 'theloai3',
-          //         child: Text('Thể loại 3'),
-          //       ),
-          //     ];
-          //   },
-          //   onSelected: (String choice) {
-          //     // Xử lý khi chọn một thể loại từ menu
-          //   },
-          // ),
+          PopupMenuButton<String>(
+            itemBuilder: (BuildContext context) {
+              return <PopupMenuEntry<String>>[
+                PopupMenuItem(
+                  value: 'theloai1',
+                  child: Text('Thể loại 1'),
+                ),
+                PopupMenuItem(
+                  value: 'theloai2',
+                  child: Text('Thể loại 2'),
+                ),
+                PopupMenuItem(
+                  value: 'theloai3',
+                  child: Text('Thể loại 3'),
+                ),
+              ];
+            },
+            onSelected: (String choice) {
+              // Xử lý khi chọn một thể loại từ menu
+            },
+          ),
           // IconButton(
           //   icon: Icon(
           //     Icons.exit_to_app,
