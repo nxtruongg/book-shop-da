f2772ca79458b039f05585dcc86e2c185afe0b71
XTruong <nxtruong0811@gmail.com>
2023-12-03 07:10:43 +0700

    update code truong

--- lib/menudonhang.dart
+++ lib/menudonhang.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:ontap3011/models/so_models.dart';
 
 import 'dangxuly.dart';
 import 'detaillxacnhan.dart';
+import 'package:intl/intl.dart';
 
-class menudonhang extends StatelessWidget {
+class menudonhang2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -44,6 +46,88 @@ class menudonhang extends StatelessWidget {
               Center(child: Text('Đã hủy')),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+class menudonhang extends StatefulWidget {
+  const menudonhang({super.key});
+
+  @override
+  State<menudonhang> createState() => _menudonhangState();
+}
+
+class _menudonhangState extends State<menudonhang> {
+   //final So so = So();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 5, // Số lượng tab
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Color(0xFFBA1541),
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.of(context)
+                    .pop(); // Điều này sẽ đóng trang hiện tại và quay lại trang trước đó.
+              },
+            ),
+            title: Text('Trạng thái đơn hàng'),
+            bottom: TabBar(
+              tabs: [
+                Tab(text: 'Chờ xác nhận'),
+                Tab(text: 'Chờ lấy hàng'),
+                Tab(text: 'Chờ giao hàng'),
+                Tab(text: 'Đã giao'),
+                Tab(text: 'Đã hủy'),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              // Nội dung cho tab 'Chờ xác nhận'
+
+              // Nội dung cho tab 'Chờ lấy hàng'
+              Center(child: Text('Chờ lấy hàng')),
+              // Nội dung cho tab 'Chờ giao hàng'
+              Center(child: Text('Chờ giao hàng')),
+              // Nội dung cho tab 'Đã giao'
+
+              // Nội dung cho tab 'Đã hủy'
+              Center(child: Text('Đã hủy')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class OderItem extends StatelessWidget {
+  final So order;
+
+
+  OderItem({
+    required this.order,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(8.0),
+      child: ListTile(
+        title: Text("Số đơn hàng: ${order.so_ct} "),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Trạng thái: ${order.ten_trang_thai}"),
+            Text("Tổng tiền: ${order.tong_tien}"),
+            Text(
+                "Ngày mua: ${DateFormat('dd/MM/yyyy').format(order.date_created)}"),
+          ],
         ),
       ),
     );
--- lib/models/cartmodels.dart
+++ lib/models/cartmodels.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:dio/dio.dart';
 
 class CartObj {
@@ -7,17 +8,16 @@ class CartObj {
   final int gia_ban;
   final int sl;
   final String image;
-
+  final int tien_ck;
   CartObj(
       {required String id,
       required this.id_sp,
       required this.ten_sp,
       required this.gia_ban,
       this.sl = 0,
-      this.image = 'images/logo.png'}) {
+      this.image = 'images/logo.png',this.tien_ck = 0,}) {
     _id = id;
   }
-
   factory CartObj.fromJson(Map<String, dynamic> json) {
     return CartObj(
       id: json['_id'],
@@ -26,13 +26,13 @@ class CartObj {
       gia_ban: json['gia_ban'] ?? 0.0,
       sl: json['sl'] ?? 0,
       image: json['image'] ?? 'images/logo.png',
+      tien_ck: json['tien_ck']??0
     );
   }
   @override
   String toString() {
-    return 'CartObj{_id:$_id,id_sp: $id_sp, ten_sp: $ten_sp, gia_ban: $gia_ban, sl: $sl, image: $image}';
+    return 'CartObj{_id:$_id,id_sp: $id_sp, ten_sp: $ten_sp, gia_ban: $gia_ban, sl: $sl, image: $image,tien_ck:$tien_ck}';
   }
-
   Map<String, dynamic> toJson() {
     return {
       '_id': _id,
@@ -41,19 +41,17 @@ class CartObj {
       'gia_ban': gia_ban,
       'sl': sl,
       'image': image,
+      'tien_ck':tien_ck,
       // Thêm các trường khác nếu có
     };
   }
 }
-
 class CartModels {
   final Dio dio = Dio();
-
   Future<List<CartObj>> getCart() async {
     try {
       final response = await dio.get(
           'https://api.goodapp.vn/api/648deb5c4992aaaa9b8a165d/cartapp?access_token=70f4bbed48a5d186e31323b2136f15a7');
-      print('data ${response.data}');
       return List<CartObj>.from(
           response.data.map((dt) => CartObj.fromJson(dt)));
     } catch (e) {
@@ -61,7 +59,6 @@ class CartModels {
       throw e;
     }
   }
-
   Future<bool> deleteItemCart(CartObj item) async {
     try {
       print(item);
@@ -70,6 +67,43 @@ class CartModels {
       return true;
     } catch (e) {
       throw e;
+    }
+  }
+  Future<void> addItemtoCart(int? id,int? price,int? ck) async {
+    try {
+      Map<String, dynamic> condition = {
+        'id_sp':id,
+      };
+      Map<String, dynamic> data ={};
+      final rs = await dio.get(
+          'https://api.goodapp.vn/api/648deb5c4992aaaa9b8a165d/cartapp?access_token=70f4bbed48a5d186e31323b2136f15a7&q=${jsonEncode(condition)}');
+      print(rs.data);
+      if(rs.data.length >0){
+        data['sl'] =  (rs.data![0]['sl']??0) + 1;
+        print('data $data');
+        final rs_update = await dio.put('https://api.goodapp.vn/api/648deb5c4992aaaa9b8a165d/cartapp/${rs.data[0]['_id']}?access_token=70f4bbed48a5d186e31323b2136f15a7',data:json.encode(data));
+      print('put data ${rs_update.data}');
+      } else {
+        data['id_sp'] = id;
+        data['sl']= 1;
+        data['gia_ban'] =price??0;
+        data['tien_ck'] = ck??0;
+        final rs_post = await dio.post('https://api.goodapp.vn/api/648deb5c4992aaaa9b8a165d/cartapp?access_token=70f4bbed48a5d186e31323b2136f15a7',data: json.encode(data));
+        print('post data ${rs_post.data}');
+      }
+    }catch(e){
+    throw Exception(e);
+    }
+  }
+  Future<void> UpdateItemCart(CartObj item,int so_luong) async{
+    try{
+      Map<String, dynamic> data ={
+        'sl':so_luong
+      };
+      final rs = await dio.put('https://api.goodapp.vn/api/648deb5c4992aaaa9b8a165d/cartapp/${item._id}?access_token=70f4bbed48a5d186e31323b2136f15a7',data:json.encode(data));
+      print(rs.data);
+    }catch(e){
+      throw Exception(e);
     }
   }
 }
